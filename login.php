e0392fceeab0c885cf8a2c7cf31094aef5fee09f
ismoil1991 <divergent9119@gmail.com>
2021-03-21 16:22:30 +0500

    save to session auth user

--- login.php
+++ login.php
@@ -5,18 +5,20 @@ include 'functions.php';
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$user = login($email,$password);
+$user = login($email, $password);
 
-if ($user){
+if ($user) {
     $name = 'success';
     $message = 'OK';
-    set_flash_message($name,$message);
+    set_flash_message($name, $message);
+
+    set_session();
 
     redirect_to('Location: /users.php');
 } else {
     $name = 'danger';
     $message = 'Неправильный пароль или email';
-    set_flash_message($name,$message);
+    set_flash_message($name, $message);
 
     redirect_to('Location: /page_login.php');
 }
