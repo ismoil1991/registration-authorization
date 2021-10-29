500f2224f0da61ebc38dc28344787d007fe6fd42
ismoil1991 <divergent9119@gmail.com>
2021-10-29 11:29:04 +0500

    status handle

---
+++ status_handle.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+include "functions.php";
+
+$edit = set_status($_POST['status'], $_SESSION['user']['id']);
+
+if ($edit){
+    $message = '<strong>Успешно изменен</strong>';
+    $name ='success';
+    set_flash_message($name, $message);
+
+    redirect_to('Location: /page_profile.php?id='.$_SESSION['user']['id']);
+} else {
+    $message = '<strong>Что-то пошло не так</strong>';
+    $name ='danger';
+    set_flash_message($name, $message);
+
+    redirect_to('Location: /edit.php?id='.$_SESSION['user']['id']);
+}
